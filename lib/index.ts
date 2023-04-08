02615bfa8890cd7dd54ccb0f1f9dc51ea06c0c72
ItMaga <m-chemurziev@ya.ru>
2023-04-08 16:07:43 +0300

    types: specify `createMock` return type

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,7 @@
 import type { z } from 'zod';
 import MockGenerator from './MockGenerator';
 
-function createMock<Schema extends z.ZodTypeAny>(schema: Schema) {
+function createMock<Schema extends z.ZodTypeAny>(schema: Schema): z.infer<typeof schema> {
   const mockGenerator = new MockGenerator(schema);
   return mockGenerator.generate();
 }

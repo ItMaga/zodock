350ef68d06f9dd415113a5893e3a2c77fbacf697
ItMaga <m-chemurziev@ya.ru>
2023-04-08 19:18:05 +0300

    fix: BaseGenerator import

--- lib/generators/NullableGenerator.ts
+++ lib/generators/NullableGenerator.ts
@@ -1,5 +1,6 @@
 import type { z } from 'zod';
 import MockGenerator from '../MockGenerator';
+import type BaseGenerator from './BaseGenerator';
 
 export default class NullableGenerator<TOptions extends z.ZodTypeAny, T extends z.ZodNullable<TOptions>> implements BaseGenerator<T> {
   public generate(schema: T) {

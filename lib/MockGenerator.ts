8d334b072ff43e200dcba773fc1e7bf329c46c02
ItMaga <m-chemurziev@ya.ru>
2023-04-08 16:08:58 +0300

    feat: replace `instanceof` with mapper

--- lib/MockGenerator.ts
+++ lib/MockGenerator.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod';
+import type { TypeOf, ZodFirstPartyTypeKind, z } from 'zod';
 import StringGenerator from './generators/StringGenerator';
 import type BaseGenerator from './generators/BaseGenerator';
 import NumberGenerator from './generators/NumberGenerator';
@@ -15,60 +15,35 @@ import EffectsGenerator from './generators/EffectsGenerator';
 import OptionalGenerator from './generators/OptionalGenerator';
 
 export default class MockGenerator<T extends z.ZodTypeAny> {
-  private generator: BaseGenerator<z.infer<T>>;
+  private generator: BaseGenerator<TypeOf<T>>;
   private schema: T;
 
   constructor(schema: T) {
     this.schema = schema;
 
-    if (this.schema instanceof z.ZodString) {
-      this.generator = new StringGenerator();
+    const generatorMap: Partial<Record<ZodFirstPartyTypeKind, any>> = {
+      ZodString: StringGenerator,
+      ZodNumber: NumberGenerator,
+      ZodBoolean: BooleanGenerator,
+      ZodLiteral: LiteralGenerator,
+      ZodDate: DateGenerator,
+      ZodNull: NullGenerator,
+      ZodUndefined: UndefinedGenerator,
+      ZodAny: StringGenerator,
+      ZodUnknown: StringGenerator,
+      ZodObject: ObjectGenerator,
+      ZodArray: ArrayGenerator,
+      ZodUnion: UnionGenerator,
+      ZodTuple: TuppleGenerator,
+      ZodEffects: EffectsGenerator,
+      ZodOptional: OptionalGenerator,
+    };
+    if (this.schema._def.typeName in generatorMap) {
+      this.generator = new generatorMap[this.schema._def.typeName as ZodFirstPartyTypeKind]();
+      return;
     }
-    else if (this.schema instanceof z.ZodNumber) {
-      this.generator = new NumberGenerator();
-    }
-    else if (this.schema instanceof z.ZodBoolean) {
-      this.generator = new BooleanGenerator();
-    }
-    else if (this.schema instanceof z.ZodLiteral) {
-      this.generator = new LiteralGenerator();
-    }
-    else if (this.schema instanceof z.ZodDate) {
-      this.generator = new DateGenerator();
-    }
-    else if (this.schema instanceof z.ZodNull) {
-      this.generator = new NullGenerator();
-    }
-    else if (this.schema instanceof z.ZodUndefined) {
-      this.generator = new UndefinedGenerator();
-    }
-    else if (this.schema instanceof z.ZodAny) {
-      this.generator = new StringGenerator();
-    }
-    else if (this.schema instanceof z.ZodUnknown) {
-      this.generator = new StringGenerator();
-    }
-    else if (this.schema instanceof z.ZodObject) {
-      this.generator = new ObjectGenerator();
-    }
-    else if (this.schema instanceof z.ZodArray) {
-      this.generator = new ArrayGenerator();
-    }
-    else if (this.schema instanceof z.ZodUnion) {
-      this.generator = new UnionGenerator();
-    }
-    else if (this.schema instanceof z.ZodTuple) {
-      this.generator = new TuppleGenerator();
-    }
-    else if (this.schema instanceof z.ZodEffects) {
-      this.generator = new EffectsGenerator();
-    }
-    else if (this.schema instanceof z.ZodOptional) {
-      this.generator = new OptionalGenerator();
-    }
-    else {
-      throw new TypeError(`Unsupported schema type: ${this.schema.constructor.name}`);
-    }
+
+    throw new TypeError(`Unsupported schema type: ${this.schema.constructor.name}`);
   }
 
   public generate(): z.infer<T> {

f5ed1e8d92b08f0c09ebd94eb74b19ab84f658a7
ItMaga <m-chemurziev@ya.ru>
2023-04-13 02:53:17 +0300

    feat: add `changelogithub` config

---
+++ changelogithub.config.ts
@@ -0,0 +1,17 @@
+export default {
+  scopeMap: {},
+  types: {
+    feat: { title: '🚀 Features' },
+    fix: { title: '🐞 Bug Fixes' },
+    perf: { title: '🏎 Performance' },
+    docs: { title: '📝 Documentation' },
+    test: { title: '🧪 Tests' },
+    refactor: { title: '🛠 Refactors' },
+  },
+  titles: {
+    breakingChanges: '🚨 Breaking Changes',
+  },
+  contributors: true,
+  capitalize: true,
+  group: true,
+};

b63b909625025738dfad77ea127053abe261fca4
ItMaga <m-chemurziev@ya.ru>
2023-04-22 23:57:28 +0300

    refactor: use declarative access to branded type

--- lib/generators/BrandedGenerator.ts
+++ lib/generators/BrandedGenerator.ts
@@ -4,7 +4,7 @@ import type BaseGenerator from './BaseGenerator';
 
 export default class BrandedGenerator<T extends z.ZodBranded<z.ZodTypeAny, string | number | symbol>> implements BaseGenerator<T> {
   public generate(schema: T): z.infer<typeof schema> {
-    const mockGenerator = new MockGenerator(schema._def.type);
+    const mockGenerator = new MockGenerator(schema.unwrap());
     return mockGenerator.generate();
   }
 }
